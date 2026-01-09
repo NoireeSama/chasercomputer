38f0d8f97d45b627737be9f289f5699701e1547e
mirzardy <mirza.ok0010@gmail.com>
2026-01-09 16:07:54 +0700

    in tes merge

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PesananController;
 use App\Http\Controllers\Api\DetailPesananController;
 
-Route::prefix('pesanan')->gr
-Route::post('/detail-pesanan', [DetailPesananController::class, 'store']);
-oup(function () {
-    Route::post('/', [PesananController::class, 'store']);
-    Route::put('{id}', [PesananController::class, 'update']);
+
     Route::delete('{id}', [PesananController::class, 'destroy']);
-});
+
