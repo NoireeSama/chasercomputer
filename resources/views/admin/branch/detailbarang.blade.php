fe50e5e08da2148a0f9783d03325ac6123e2bf70
NoireeSama <noireesama@users.noreply.github.com>
2026-01-10 15:17:04 +0700

    tambah edit text harga barang

--- resources/views/admin/branch/detailbarang.blade.php
+++ resources/views/admin/branch/detailbarang.blade.php
@@ -84,6 +84,13 @@
                             <svg class="edit-icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                         </div>
                     </div>
+                    <div class="form-group">
+                        <label>Harga Barang :</label>
+                        <div class="glass-pill input-wrapper">
+                            <input type="text" value="Lenovo LOQ 15 IAX9i" placeholder="Masukkan harga barang...">
+                            <svg class="edit-icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
